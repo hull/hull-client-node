f289cfc97134ea38f59cdf83f44fe69fc5716cc8
Romain Dardour <romain@hull.io>
2017-06-20 14:44:13 +0200

    update demo script

--- demo.js
+++ demo.js
@@ -1,44 +1,54 @@
-require('babel-register');
+require("babel-register");
 // var Hull = require('./lib/index.js');
-var Hull = require('./src/index.js');
+const Hull = require("./src/index.js");
 
 if (process.env.HULL_ID && process.env.HULL_SECRET && process.env.HULL_ORGANIZATION) {
-  var hull = new Hull({
+  const hull = new Hull({
     id: process.env.HULL_ID,
     secret: process.env.HULL_SECRET,
     organization: process.env.HULL_ORGANIZATION
   });
 
-  hull.get('/org').then(function(data) {
-    console.log('Org Name');
+  hull.get("/org").then(function (data) {
+    console.log("Org Name");
     console.log(data.name);
-    console.log('-------\n');
-  }).catch(function(err) {
+    console.log("-------\n");
+  }).catch(function (err) {
     console.log(err);
   });
-  hull.get('/org/comments').then(function(data) {
-    console.log('Comments');
+  hull.get("/org/comments").then(function (data) {
+    console.log("Comments");
     console.log(data);
-    console.log('-------\n');
-  }).catch(function(err) {
+    console.log("-------\n");
+  }).catch(function (err) {
     console.log(err);
   });
 
-  var me = hull.as(process.env.HULL_ME_TEST);
+  const me = hull.asUser({ id: process.env.HULL_ME_TEST });
 
-  me.get('/me').then(function(data) {
-    console.log('/me email for ' + process.env.HULL_ME_TEST);
-    console.log(data.email);
-    console.log('-------\n');
+  me.get("/me").then(function (data) {
+    console.log(`/me email for ${process.env.HULL_ME_TEST} : ${data.email}`);
+    console.log("-------\n");
   });
-  me.get('/me/liked/5103a55193e74e3a1f00000f').then(function(data) {
-    console.log('Did I Like Org', data);
-    console.log('-------\n');
-  }).catch(function(err){
-    console.log(err);
-    console.log('-------\n');
+
+  hull.post("search/user_reports", {
+    query: {
+      bool: {
+        should: [
+          { term: { "email.exact": process.env.HULL_EMAIL_TEST } }
+        ],
+        minimum_should_match: 1
+      }
+    },
+    raw: true,
+    page: 1,
+    per_page: 1
+  }).then(function searchUser(emailUser) {
+    console.log(`Found: ${emailUser.data[0].name}`);
+  }, function searchUserError(err) {
+    console.log("Error", err);
   });
 
 } else {
-  console.log('Environment variables not set.');
+  console.log("Environment variables not set.");
 }
